836e33c5a39956668aa1be01ae7d8cb1f1507590
gashiyama <higashiyama_masahiro@cam-inc.co.jp>
2020-05-09 23:28:15 +0900

    jsxの修正

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+//jsx使用時 React が必須
 import './App.css';
 
 class App extends Component {
   render() {
-    return <h1>hello,world!</h1>;
+    //attributeはキャメルケース
+    //react.fragment
+    return (
+      <React.Fragment>
+        <label htmlFor='papakatu'>papakatu</label>
+        <input type='text' onChange={() => { console.log(11111); } }/>
+      </React.Fragment>
+    );
   }
 }
 
