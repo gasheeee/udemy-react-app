37e45e7970f05287ce19aea22c95d2c18e0b93f6
gashiyama <higashiyama_masahiro@cam-inc.co.jp>
2020-05-10 16:07:47 +0900

    prop-types example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 //jsx使用時 React が必須
+import PropTypes from 'prop-types';
 import './App.css';
 
 const App = () => {
   const profiles = [
     { name: 'gasheeee', age: 20},
     { name: 'gashiyama', age: 25 },
-    { name: 'ore' }
+    { name: 'masa' }
   ];
 
   return (
@@ -25,6 +26,11 @@ const User = props => {
     <h1>Hi! I am { props.name }, and {props.age} years old.</h1>
   );
 }
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number
+};
 
 User.defaultProps = {
   age: 10
