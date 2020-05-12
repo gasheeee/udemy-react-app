fddb898876ebabdb86f361c4b0d638e4b5f2fd37
gashiyama <higashiyama_masahiro@cam-inc.co.jp>
2020-05-13 00:24:58 +0900

    store add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
+import reducer from './redux/reducers';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
